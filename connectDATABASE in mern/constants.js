4f6d810acaf49a47821f8d7b1fe22162e4a8ba22
Adarsh Bhardwaj <adarshbhardwaj1919@gmail.com>
2025-09-06 15:34:24 +0530

    Delete connectDATABASE in mern directory

--- connectDATABASE in mern/constants.js
+++
@@ -1,3 +0,0 @@
-export const DB_NAME = "videotube" //files are kept here because they can be 
-// used in multiple files by importing from here and to change them in one place it can be done from here
-// filepath: constants.js
